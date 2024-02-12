d51b4efff63d61db474c738aa00dccf1caef5219
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-12 16:44:46 +0000

    Progress on writing new logic with array

--- calculator.js
+++ calculator.js
@@ -13,38 +13,77 @@ document.addEventListener('DOMContentLoaded', () => {
     function buttonHandler(button) {
         switch (true) {
             case button.classList.contains('num') && opData.length <= 1:
-                opData[0] += button.id;
+                opData[0] += button.textContent;
                 break;
             case button.classList.contains('num') && opData.length >= 2:
-                opData[2] += (button.id);
+                opData[2] += (button.textContent);
                 break;
-            case button.classList.contains('num') && opData.
+            case button.classList.contains('op') && opData.length == 0:
+                opData[0] = 0;
+                opData[1] = button.textContent;
+                break;
+            case button.classList.contains('op') && opData.length == 1 || opData.length == 2:
+                opData[1] = button.textContent;
+                break;
+            case button.classList.contains('op') && opData.length == 3:
+                operate();
+                break;
+            case button.id == 'equals' && opData.length == 3:
+                operate();
+                break;
+            case button.id == 'special' && opData.length != 0:
+                percent();
+                break;
+            case button.id == 'ac':
+                reset();
+                break;
+            case button.id == 'undo':
+                undo();
+                break;
         }
     }
 
-    function addition(){
+    function addition(a,b){
           
     }
 
-    function subtraction(){
+    function subtraction(a,b){
         
     }
 
-    function multiplication(){
+    function multiplication(a,b){
        
     }
 
-    function division(){
+    function division(a,b){
         
 
     }
 
-    function percent(){
+    function percent(a,b){
         
     }
 
     function operate(){
-       
+       let a = parseFloat (opData[0]);
+       let b = parseFloat (opData[2]);
+       switch (opData[1]){
+        case '+':
+            addition (a,b);
+            break;
+        case '-':
+            subtraction (a,b);
+            break;
+        case '*':
+            multiplication (a,b);
+            break;
+        case '/':
+            division (a,b);
+            break;
+        case '%':
+            percent (a,b);
+            break;
+       }
     }
 
     function reset(){
