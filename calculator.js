a0f0a51bea173c58e48d367dfc7fdb18c5fd1e7d
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-13 02:36:48 +0000

    Rewrite logic, almost finished

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('button');
     const display1 = document.getElementById('primaryDisplay');
     const display2 = document.getElementById('secondaryDisplay');
-    let opData = ['', '', ''];
+    let opData = [];
 
     buttons.forEach (button => {
         button.addEventListener('click', () => {
@@ -12,30 +12,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function buttonHandler(button) {
         switch (true) {
-            case button.classList.contains('num') && opData.length <= 1:
-                opData[0] += button.textContent;
+            case button.classList.contains('num') && opData.length == 0:
+                opData.push(button.textContent);
+                updateDisplay(0);
                 break;
-            case button.classList.contains('num') && opData.length >= 2:
+            case button.classList.contains('num') && opData.length == 1:
+                opData[0] += button.textContent;
+                updateDisplay(0); 
+                break;
+            case button.classList.contains('num') && opData.length == 2:
+                opData.push (button.textContent);
+                updateDisplay(2);
+                break;
+            case button.classList.contains('num') && opData.length == 3:
                 opData[2] += (button.textContent);
+                updateDisplay(2);
                 break;
             case button.classList.contains('op') && opData.length == 0:
-                opData[0] = 0;
-                opData[1] = button.textContent;
+                opData.push (0, button.textContent);
+                updateDisplay(1);
                 break;
-            case button.classList.contains('op') && opData.length == 1 || opData.length == 2:
-                opData[1] = button.textContent;
+            case button.classList.contains('op') && opData.length == 1:
+                opData.push (button.textContent);
+                updateDisplay(1);
                 break;
+            case button.classList.contains('op') && opData.length == 2:
+                opData[1] = button.textContent;
+                updateDisplay(1);
             case button.classList.contains('op') && opData.length == 3:
-                operate();
+                operate(opData);
+                updateDisplay(2);
                 break;
             case button.id == 'equals' && opData.length == 3:
-                operate();
+                operate(opData);
                 break;
-            case button.id == 'special' && opData.length != 0:
-                percent();
+            case button.id == 'percent' && opData.length !=0:
+                opData[1] = button.textContent;
+                operate(opData);
                 break;
             case button.id == 'ac':
                 reset();
+                updateDisplay();
                 break;
             case button.id == 'undo':
                 undo();
@@ -43,25 +60,62 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function updateDisplay(i){
+        switch (i) {
+            case 0:
+                display1.textContent = opData[0];
+                break;
+            case 1:
+                display1.textContent = opData[0] + opData[1];
+                break;
+            case 2:
+                display1.textContent = opData[0] + opData[1] + opData[2];
+                break;
+            default:
+                display1.textContent = 0;
+        }
+    }
+
     function addition(a,b){
-          
+        reset();
+        opData.push (a+b);
+        updateDisplay(0);
     }
 
     function subtraction(a,b){
-        
+        reset();
+        opData.push (a-b);
+        updateDisplay(0);
     }
 
     function multiplication(a,b){
-       
+        reset();
+        opData.push (a*b);
+        updateDisplay(0);
     }
 
     function division(a,b){
-        
+        reset();
+        if (b == 0) {
+            display1.textContent = "Self destructing in 3...2...1"
+            reset();
+        } else {
+        opData.push (a/b);
+        updateDisplay(0);
+        }
 
     }
 
     function percent(a,b){
-        
+        switch (opData.length){
+            case 1:
+                opData[1].textContent = a/100;
+                updateDisplay(0);
+                break;
+            case 3:
+                opData[2].textContent = b/100;
+                updateDisplay(2);
+        }
     }
 
     function operate(){
@@ -87,11 +141,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function reset(){
-        
+        opData = [];
     }
 
     function undo(){
-
+        switch (opData.length){
+            case 0:
+                break;
+            case 1:
+                opData[0] = opData[0].slice(0, -1);
+                updateDisplay(0);
+                break;
+            case 2:
+                break;
+            case 3:
+                opData[2] = opData[2].slice(0, -1);
+                updateDisplay(2);
+        }
     }
 
 });
